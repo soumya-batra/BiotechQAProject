266c23d4bda3a5035b1f67a3a5ccb667e23147f2
liuhaodong <hi.haodong@gmail.com>
2013-11-04 21:58:56 -0500

    Modified Sentence and Token

--- src/biotechProject/types/Sentence.java
+++ src/biotechProject/types/Sentence.java
@@ -1,5 +1,43 @@
+//Modified By Haodong 
 package biotechProject.types;
 
-public class Sentence {
+import java.util.List;
+import java.util.StringTokenizer;
 
+public class Sentence {
+	List<Token> tokenList;
+	private double sentenceScore;
+	private boolean isSelected;
+	private String textString;
+
+	public Sentence(String text) {
+		textString = text;
+		sentenceScore = 0;
+		// break down input string into tokens
+		StringTokenizer tokenizer = new StringTokenizer(text);
+		while (tokenizer.hasMoreTokens()) {
+			Token token = new Token(tokenizer.nextToken());
+			tokenList.add(token);
+		}
+	}
+
+	public void setSentenceScore(double score) {
+		sentenceScore = score;
+	}
+
+	public double getSentenceScore() {
+		return sentenceScore;
+	}
+
+	public void setIsSelected(boolean isSelected) {
+		this.isSelected = isSelected;
+	}
+
+	public boolean getIsSelected() {
+		return isSelected;
+	}
+
+	public String getTextString() {
+		return textString;
+	}
 }
